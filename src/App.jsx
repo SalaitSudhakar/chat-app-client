4fff769271c95e16be8995ace4cd387d963636ed
SalaitSudhakar <salaitsudhakar.s@gmail.com>
2025-05-02 15:55:08 +0530

    Add axios and zustand package and axios config file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import React from "react";
-import BrowserRouter from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Home from "./Pages/Home";
+import Signup from "./Pages/Signup";
+import Login from "./Pages/Login";
+import Profile from "./Pages/Profile";
+import Settings from "./Pages/Settings";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-      
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
