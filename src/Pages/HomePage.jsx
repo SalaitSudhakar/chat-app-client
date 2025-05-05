4836afe2b9d73187b3a762421aec9e9b1353072a
SalaitSudhakar <salaitsudhakar.s@gmail.com>
2025-05-05 22:23:18 +0530

    Add welcomeChatMessage file content

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { useChatStore } from '../Store/useChatStore';
+import WelcomeChatMessage from '../Components/WelcomeChatMessage';
+import ChatContainer from '../Components/ChatContainer';
+import Sidebar from '../Components/Sidebar';
 
 const HomePage = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const { selectedUser } = useChatStore();
 
-export default HomePage
+  return (
+    <div className=" px-4 lg:-mt-20 pt-16 bg-base-100">
+      <div className="flex items-center justify-center h-full">
+        <div className="bg-base-100 rounded-lg shadow-xl w-full max-w-6xl flex h-[calc(100vh-8rem)]">
+          <Sidebar />
+          {selectedUser ? <ChatContainer /> : <WelcomeChatMessage />}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
